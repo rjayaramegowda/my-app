5b7f325643088bbda31c93d3c880124a8d3836ad
Ravichandran Jayaramegowda <rjayaramegowda@gmail.com>
2022-10-15 15:14:49 +0400

    Added React Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <h1 className='mb-2'>"react-bootstrap": "^2.5.0"</h1>
+        <h1>"bootstrap": "^5.2.2", </h1>
+        <Button>Primary Button</Button>
+      </Container>
+    </>
   );
 }
 
